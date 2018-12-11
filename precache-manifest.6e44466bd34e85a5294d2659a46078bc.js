da32060d9fb4e6bca9ded0d528a7dc40f55a950a
andiloveher <andiloveher@live.com>
2018-12-11 15:54:18 +0800

    Updates

---
+++ precache-manifest.6e44466bd34e85a5294d2659a46078bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d065f651390851d514a",
+    "url": "/andiloveher/reactblog/static/js/runtime~main.9d065f65.js"
+  },
+  {
+    "revision": "f1a39d27beea10cc4830",
+    "url": "/andiloveher/reactblog/static/js/main.f1a39d27.chunk.js"
+  },
+  {
+    "revision": "d4d3aeb0feacf48dd3f2",
+    "url": "/andiloveher/reactblog/static/js/1.d4d3aeb0.chunk.js"
+  },
+  {
+    "revision": "f1a39d27beea10cc4830",
+    "url": "/andiloveher/reactblog/static/css/main.d699453e.chunk.css"
+  },
+  {
+    "revision": "d4d3aeb0feacf48dd3f2",
+    "url": "/andiloveher/reactblog/static/css/1.c723ab93.chunk.css"
+  },
+  {
+    "revision": "a254d277f98c8b4ced54fd4255f5bafb",
+    "url": "/andiloveher/reactblog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/andiloveher/andiloveher.github.com/precache-manifest.5a796be381579775e763c1a9485facc5.js"
+  "/andiloveher/reactblog/precache-manifest.6e44466bd34e85a5294d2659a46078bc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/andiloveher/andiloveher.github.com/index.html", {
+workbox.routing.registerNavigationRoute("/andiloveher/reactblog/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
